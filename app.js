692af6aa0b0561e116506f001b65457fe3714b0d
Yao Min <ngyaominsg@gmail.com>
2018-05-07 21:44:09 +0800

    Adding the loader

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  document.getElementById('results').style.display = 'none';
+  
+  document.getElementById('loading').style.display = 'block';
+  
+  setTimeout(calculateResults, 2000);
+  
+  e.preventDefault();
+});
 
 // calculate Results
-function calculateResults(e) {
+function calculateResults() {
   console.log('calculating... ')
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -23,15 +31,21 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+    
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('please check your numbers')
   }
-  
-  e.preventDefault();
 }
 
 // Show error
-function showError() {
+function showError(error) {
+  
+  document.getElementById('results').style.display = 'none';
+  
+  document.getElementById('loading').style.display = 'none';
+  
   const errorDiv = document.createElement('div');
   
   const card = document.querySelector('.card');
